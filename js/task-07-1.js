2ab2c000b6338562a0cba6e7d80cdbe908e9185a
Dana Project <70840490+Danaproject@users.noreply.github.com>
2020-12-07 23:31:56 +0100

    +task-07 with HTML

---
+++ js/task-07-1.js
@@ -0,0 +1,50 @@
+const TransactionType = {
+   DEPOSIT: 'deposit',
+   WITHDRAW: 'withdraw',
+};
+
+const account = {
+    balance: 0,
+    transactions: [],
+
+    createTransaction(amount, type) {
+        return { id: this.transactions.length+1, amount: amount, type: type };
+    },
+    deposit(amount) {
+        return this.transactions.push(this.createTransaction(Number(amount), TransactionType.DEPOSIT));
+    },
+    withdraw(amount) {
+        return this.transactions.push(this.createTransaction(Number(amount), TransactionType.WITHDRAW));
+    },
+    getBalance() {
+        let balance=0;
+        for (const transaction of this.transactions) {
+            if (transaction.type === TransactionType.DEPOSIT) { balance += Number(transaction.amount); }
+            if (transaction.type === TransactionType.WITHDRAW) { balance -= Number(transaction.amount); }
+        }
+        return Number(balance);
+    },
+    getTransactionDetails(id) {
+        for (const transaction of this.transactions) {
+            if (transaction.id === Number(id)) { return transaction; }
+        }
+    },
+    getTransactionTotal(type) {
+        let sum = 0;
+        for (const transaction of this.transactions) {
+            if (transaction.type === type) { sum += Number(transaction.amount); }
+        }
+        return Number(sum);
+    },
+    getTransactionHTML() {
+        let result = "<table border=1>";
+        result += "<tr><td>id</td><td>amount</td><td>type</td></tr>";
+        for (const transaction of this.transactions) {
+            result += "<tr><td>"+transaction.id+"</td><td>"+transaction.amount+"</td><td>"+transaction.type+"</td></tr>";
+        }
+        result += "</table>"
+        return result;
+    },
+    
+}
+export default account;
---
+++ js/view-07-1.js
@@ -0,0 +1,40 @@
+import account from './task-07-1.js';
+
+
+document.querySelector("button[data-product='deposit-button']").addEventListener('click', () => {
+    let inputValue = document.getElementById("amount").value;
+    account.deposit(inputValue);
+    
+    console.table(account.transactions);
+    document.getElementById("transactions-table").innerHTML = account.getTransactionHTML();
+});
+
+document.querySelector("button[data-product='withdraw-button']").addEventListener('click', () => {
+    let inputValue = document.getElementById("amount").value;
+    account.withdraw(inputValue);
+    
+    console.table(account.transactions);
+    document.getElementById("transactions-table").innerHTML = account.getTransactionHTML();
+});
+
+document.querySelector("button[data-product='balance-button']").addEventListener('click', () => {
+    let currentBalance = account.getBalance();
+    document.getElementById("balance-field").innerText = Number(currentBalance); 
+    console.log(account.getBalance());
+    document.getElementById("transactions-table").innerHTML = account.getTransactionHTML();
+});
+
+document.querySelector("button[data-product='details-button']").addEventListener('click', () => {
+    let inputValue = document.getElementById("details").value;
+    let tr = account.getTransactionDetails(inputValue);
+    document.getElementById("details-field").innerHTML = String(`<table border="1"><tr><td>id</td><td>amount</td><td>type</td></tr><tr><td>${tr.id}</td><td>${tr.amount}</td><td>${tr.type}</td></tr></table>`);
+    console.log(tr);
+    document.getElementById("transactions-table").innerHTML = account.getTransactionHTML();
+});
+document.querySelector("button[data-product='total-button']").addEventListener('click', () => {
+    let inputValue = document.getElementById("type").value;
+    let total = account.getTransactionTotal(inputValue);
+    document.getElementById("total-field").innerText = total;
+    console.log(total);
+    document.getElementById("transactions-table").innerHTML = account.getTransactionHTML();
+});
